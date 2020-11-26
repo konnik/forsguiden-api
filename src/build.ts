536ea4df4ce320351c9edd3b4a79d7a3a9672fd4
Niklas Konstenius <niklas.konstenius@lm.se>
2020-11-26 15:41:57 +0100

    egen db-modul

--- src/build.ts
+++ src/build.ts
@@ -1,91 +1,18 @@
-import { readFileSync } from "fs"
-import { load } from "js-yaml"
 
-interface Vattendrag {
-    id: number;
-    namn: string;
-    beskrivning: string;
-}
-
-interface Forsstracka {
-    id: number;
-    namn: string;
-    klass: string;
-    langd: number;
-    fallhojd: number;
-    smhi: number;
-    flode: {
-        min: number;
-        opt: number;
-        max: number;
-    };
-}
-
-interface Lan {
-    id: number;
-    namn: string;
-}
-
-class Db {
-    lanMap: Map<number, Lan> = new Map()
-    vattendragMap: Map<number, Vattendrag> = new Map()
-    forsstrackaMap: Map<number, Forsstracka> = new Map()
-
-    lan(id: number): Lan | undefined {
-        return this.lanMap.get(id)
-    }
-
-    vattendrag(id: number): Vattendrag | undefined {
-        return this.vattendragMap.get(id)
-    }
-
-    forsstracka(id: number): Forsstracka | undefined {
-        return this.forsstrackaMap.get(id)
-    }
-
-    laddaLan(filename: string): Lan {
-        const lan = load(readFileSync(filename, { encoding: "utf-8" }))
-        this.kollaDublett(this.lan(lan.id), lan)
-        this.lanMap.set(lan.id, lan)
-        return lan
-    }
-
-    laddaVattendrag(filename: string): Vattendrag {
-        const vattendrag = load(readFileSync(filename, { encoding: "utf-8" }))
-        this.kollaDublett(this.vattendrag(vattendrag.id), vattendrag)
-        this.vattendragMap.set(vattendrag.id, vattendrag)
-        return vattendrag
-    }
-
-    kollaDublett(existerande: any, nytt: any): void {
-        if (existerande) {
-            console.error("Duplicerat id!");
-            console.error(existerande);
-            console.error(nytt);
-            process.exit(1)
-        }
-    }
-
-    laddaForsstracka(filename: string): Forsstracka {
-        const forsstracka = load(readFileSync(filename, { encoding: "utf-8" }))
-        this.kollaDublett(this.forsstracka(forsstracka.id), forsstracka)
-
-        this.forsstrackaMap.set(forsstracka.id, forsstracka)
-        return forsstracka
-    }
-
-}
+import { initDb } from "./db"
 
 
-const db: Db = new Db()
+const db = initDb()
 
 const gavleborg = db.laddaLan("./data/gavleborg/data.yml");
 const gavlean = db.laddaVattendrag("./data/gavleborg/gavlean/data.yml");
 const testeboan = db.laddaVattendrag("./data/gavleborg/testeboan/data.yml");
 const konserthuset = db.laddaForsstracka("./data/gavleborg/gavlean/konserthuset/data.yml");
 
+
 console.log(gavleborg);
 console.log(gavlean);
 console.log(testeboan);
 console.log(konserthuset)
+
 
