af3c6973f2e86cbf3b9cea1751bebdea5cd95e56
Niklas Konstenius <niklas.konstenius@lm.se>
2020-11-26 15:48:04 +0100

    hårdkoda lite mer

--- src/build.ts
+++ src/build.ts
@@ -5,14 +5,21 @@ import { initDb } from "./db"
 const db = initDb()
 
 const gavleborg = db.laddaLan("./data/gavleborg/data.yml");
+
 const gavlean = db.laddaVattendrag("./data/gavleborg/gavlean/data.yml");
-const testeboan = db.laddaVattendrag("./data/gavleborg/testeboan/data.yml");
 const konserthuset = db.laddaForsstracka("./data/gavleborg/gavlean/konserthuset/data.yml");
 
+const testeboan = db.laddaVattendrag("./data/gavleborg/testeboan/data.yml");
+const brannsagen = db.laddaForsstracka("./data/gavleborg/testeboan/brannsagen-abyggeby/data.yml");
+const vavaren = db.laddaForsstracka("./data/gavleborg/testeboan/vavaren/data.yml");
+const forsby = db.laddaForsstracka("./data/gavleborg/testeboan/forsby/data.yml");
 
 console.log(gavleborg);
 console.log(gavlean);
-console.log(testeboan);
 console.log(konserthuset)
+console.log(testeboan);
+console.log(brannsagen);
+console.log(vavaren);
+console.log(forsby);
 
 
